ed08bae06090da1343bc8b2aca115939a31018a8
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-04-28 13:58:52 +0100

    Commit updated unity project

---
+++ Arduino Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Color currentColour;
+    public float lightTimer;
+    public string[] wireColours = { "Red", "Green", "Blue", "Yellow" };
+
+    private Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        ChooseRandomWire();
+    }
+
+
+
+    void ChooseRandomWire()
+    {
+        string chosenWire = wireColours[Random.Range(0, wireColours.Length)];
+        if (chosenWire == "Red")
+        {
+            Debug.Log("Red wire has been selected.");
+            RedWire();
+        }
+        if (chosenWire == "Green")
+        {
+            Debug.Log("Green wire has been selected");
+            GreenWire();
+        }
+        if (chosenWire == "Yellow")
+        {
+            Debug.Log("Yellow wire has been selected"); 
+            YellowWire();
+        }
+        if (chosenWire == "Blue")
+        {
+            Debug.Log("Blue wire has been selected"); 
+            BlueWire();
+        }
+    }
+
+    void RedWire()
+    {
+        anim.SetBool("RedLight", true);
+    }
+
+
+    void GreenWire()
+    {
+        anim.SetBool("GreenLight", true);
+    }
+
+    void YellowWire()
+    {
+        anim.SetBool("YellowLight", true);
+    }
+
+    void BlueWire()
+    {
+        anim.SetBool("BlueLight", true);
+    }
+}
---
+++ Arduino Project/Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public static int scoreAmount;
+    private Text scoreText;
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreAmount = 0;
+    }
+
+    void Update()
+    {
+        scoreText.text = "SCORE: " + scoreAmount;
+    }
+}
